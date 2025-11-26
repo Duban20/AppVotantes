d12a359d13aba7fd118dd8a5432dc7616fd773a9
Duban20 <dubanmb@gmail.com>
2025-11-26 17:40:48 -0500

    .

--- sitevotantes/settings.py
+++ sitevotantes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'appformulario.apps.AppformularioConfig',
     'AppPuestoVotacion.apps.AppPuestoVotacionConfig',
     'AppMunicipio.apps.AppMunicipioConfig',
