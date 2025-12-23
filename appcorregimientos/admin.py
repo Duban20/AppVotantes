3362fde6889a8f488268e164172e9e133ba2f499
Duban20 <dubanmb18@gmail.com>
2025-12-22 22:50:26 -0500

    mejoras al admin

--- appcorregimientos/admin.py
+++ appcorregimientos/admin.py
@@ -1,19 +1,67 @@
 from django.contrib import admin
-# from .models import Corregimiento
+from .models import Corregimiento
 
-# class CorregimientoAdmin(admin.ModelAdmin):
-#     list_display = ('nombre', 'municipio', 'status')
-#     search_fields = ('nombre', 'municipio__nombre', 'municipio__departamento__nombre')
-#     list_filter = ('status', 'municipio__departamento')
-#     ordering = ('nombre',)
+class CorregimientoAdmin(admin.ModelAdmin):
 
-#     autocomplete_fields = ('municipio',)
+# ---------- LISTADO ----------
+    list_display = (
+        'nombre',
+        'municipio',
+        'get_departamento',
+        'status',
+    )
 
-#     fieldsets = (
-#         ('Información del Corregimiento', {
-#             'fields': ('nombre', 'municipio', 'status'),
-#         }),
-#     )
+    list_filter = (
+        'status',
+        'municipio__departamento',
+    )
+
+    search_fields = (
+        'nombre',
+        'municipio__nombre',
+        'municipio__departamento__nombre',
+    )
+
+    ordering = ('nombre',)
+
+    autocomplete_fields = ('municipio',)
+
+    # ---------- OPTIMIZACIÓN ----------
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related(
+            'municipio',
+            'municipio__departamento',
+        )
+
+    # ---------- COLUMNAS PERSONALIZADAS ----------
+    @admin.display(description='Departamento', ordering='municipio__departamento__nombre')
+    def get_departamento(self, obj):
+        return obj.municipio.departamento
+
+    # ---------- FIELDSETS DINÁMICOS ----------
+    def get_fieldsets(self, request, obj=None):
+        if obj is None:
+            # CREAR (sin estado)
+            return (
+                ('Información del Corregimiento', {
+                    'fields': (
+                        'nombre',
+                        'municipio',
+                    )
+                }),
+            )
+        else:
+            # EDITAR (con estado)
+            return (
+                ('Información del Corregimiento', {
+                    'fields': (
+                        'nombre',
+                        'municipio',
+                        'status',
+                    )
+                }),
+            )
 
 
-# admin.site.register(Corregimiento, CorregimientoAdmin)
+admin.site.register(Corregimiento, CorregimientoAdmin)
